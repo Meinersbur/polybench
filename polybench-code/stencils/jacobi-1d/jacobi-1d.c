ed4f2cbc16c27e1b676208d2c89b091cae956533
tomofumi-yuki <tomofumi-yuki@8a0be867-cad2-4629-9b38-09e595722795>
2015-03-18 12:34:59 +0000

    fixed a bug with loop iterators in jacobi-2d; updated README
    git-svn-id: svn://svn.code.sf.net/p/polybench/code/trunk@55 8a0be867-cad2-4629-9b38-09e595722795

--- polybench-code/stencils/jacobi-1d/jacobi-1d.c
+++ polybench-code/stencils/jacobi-1d/jacobi-1d.c
@@ -57,15 +57,15 @@ void kernel_jacobi_1d(int tsteps,
 			    DATA_TYPE POLYBENCH_1D(A,N,n),
 			    DATA_TYPE POLYBENCH_1D(B,N,n))
 {
-  int t, i, j;
+  int t, i;
 
 #pragma scop
   for (t = 0; t < _PB_TSTEPS; t++)
     {
       for (i = 1; i < _PB_N - 1; i++)
 	B[i] = 0.33333 * (A[i-1] + A[i] + A[i + 1]);
-      for (j = 1; j < _PB_N - 1; j++)
-	A[j] = 0.33333 * (B[i-1] + B[i] + B[i + 1]);
+      for (i = 1; i < _PB_N - 1; i++)
+	A[i] = 0.33333 * (B[i-1] + B[i] + B[i + 1]);
     }
 #pragma endscop
 
