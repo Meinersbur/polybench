2dd5202a8a5a250ce57ca7516ff69f10f74d32fa
tomofumi-yuki <tomofumi-yuki@8a0be867-cad2-4629-9b38-09e595722795>
2015-12-14 15:18:34 +0000

    fixed a typo in the omp pragma reduction clause in polybench_flush_cache (reported by Sven Verdoolaege)
    git-svn-id: svn://svn.code.sf.net/p/polybench/code/trunk@73 8a0be867-cad2-4629-9b38-09e595722795

--- polybench-code/utilities/polybench.c
+++ polybench-code/utilities/polybench.c
@@ -82,7 +82,7 @@ void polybench_flush_cache()
   int i;
   double tmp = 0.0;
 #ifdef _OPENMP
-#pragma omp parallel for reduction(tmp:+) private(i)
+#pragma omp parallel for reduction(+:tmp) private(i)
 #endif
   for (i = 0; i < cs; i++)
     tmp += flush[i];
